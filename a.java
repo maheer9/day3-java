72fe2afdcdda0a687531c5ea475c54474136f222
maheer9 <ammudarlingmaheer@gmail.com>
2024-12-04 13:17:10 +0530

    Add files via upload

---
+++ a.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class a{
+    public static void main(String[] args){
+        int a=4;
+        int pos=2;
+        if (a&(pos==1)){
+            System.out.println("Set Bit");
+        }
+        else{
+            System.out.println("Not Set Bit");
+        }
+        }
+
+    }
+
+    
